a7e15526f9ccfffde9af346b35089210c45efaf5
wada-mitsuki <wada.mitsuki@suzukishouten.co.jp>
2025-01-03 12:44:13 +0900

    Suspenceの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { AppUsing } from "@/app/_components/app-using";
 import { PrivacyPolicy } from "@/app/_components/privacy-policy";
 import { TermsOfService } from "@/app/_components/terms-of-service";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Suspense } from "react";
 
 export default function Home() {
   const router = useRouter();
@@ -14,26 +15,30 @@ export default function Home() {
   };
   return (
     <div className="md:max-w-3xl md:mx-auto mb-5">
-      <Tabs
-        onValueChange={handleTabChange}
-        className="w-full mx-auto"
-        defaultValue={searchParams.get("query") || "app-using"}
-      >
-        <TabsList>
-          <TabsTrigger value="app-using">アプリ概要</TabsTrigger>
-          <TabsTrigger value="terms-of-service">利用規約</TabsTrigger>
-          <TabsTrigger value="privacy-policy">プライバシーポリシー</TabsTrigger>
-        </TabsList>
-        <TabsContent value="app-using">
-          <AppUsing />
-        </TabsContent>
-        <TabsContent value="terms-of-service">
-          <TermsOfService />
-        </TabsContent>
-        <TabsContent value="privacy-policy">
-          <PrivacyPolicy />
-        </TabsContent>
-      </Tabs>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Tabs
+          onValueChange={handleTabChange}
+          className="w-full mx-auto"
+          defaultValue={searchParams.get("query") || "app-using"}
+        >
+          <TabsList>
+            <TabsTrigger value="app-using">アプリ概要</TabsTrigger>
+            <TabsTrigger value="terms-of-service">利用規約</TabsTrigger>
+            <TabsTrigger value="privacy-policy">
+              プライバシーポリシー
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="app-using">
+            <AppUsing />
+          </TabsContent>
+          <TabsContent value="terms-of-service">
+            <TermsOfService />
+          </TabsContent>
+          <TabsContent value="privacy-policy">
+            <PrivacyPolicy />
+          </TabsContent>
+        </Tabs>
+      </Suspense>
     </div>
   );
 }
